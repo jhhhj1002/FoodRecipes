779e29bee74b2e7101e7ce718d888b40491f8a9c
_zz_hyun <jhhhj1002@hanmail.net>
2020-02-22 16:08:04 +0900

    first commit

---
+++ foodrecipes/src/main/java/com/example/demo/FoodrecipesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodrecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodrecipesApplication.class, args);
+	}
+
+}
